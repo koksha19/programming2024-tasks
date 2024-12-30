48df82c7794b4263aac8ec12e04f4b5f829af832
Lev <lev.bereza@gmail.com>
2024-12-30 17:12:58 +0200

    Changed task 1

--- task1.js
+++ task1.js
@@ -3,29 +3,40 @@
 const asyncMap = (array, callback, result) => {
   const newArray = [];
   let callbackCount = 0;
+  let errorOccurred = false;
 
   for (let i = 0; i < array.length; i++) {
+    if (errorOccurred) break;
     callback(array[i], (err, newValue) => {
-      if (err) {
-        result(err);
+      if (err && !errorOccurred) {
+        errorOccurred = true;
+        result(err, null);
       }
       newArray.push(newValue);
       callbackCount++;
       if (callbackCount === array.length) {
-        result(err, newArray);
+        result(null, newArray);
       }
     });
   }
 };
 
 asyncMap(
-  [4, 62, 242],
+  [4, 62, 'sdfdf', 242, 'sfsf'],
   (data, cb) => {
     setTimeout(() => {
-      cb(null, data * 2);
+      if (typeof data !== "number") {
+        cb(new Error('Wrong type'), null);
+      } else {
+        cb(null, data * 2);
+      }
     }, 1000);
   },
   (err, result) => {
-    console.log(err, result);
+    if (err) {
+      console.log('Error occurred: ', err);
+    } else {
+      console.log('Result: ', result);
+    }
   },
 );
